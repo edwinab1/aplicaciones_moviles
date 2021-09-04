e223211bea407f089880b1746d23fee3bd5bc02a
edwin <edwinaddres@gmail.com>
2021-09-03 20:30:41 -0400

    cambios hasta el 3 de septiembre

---
+++ app/src/main/java/com/edwinab/app1/ActividadEjemplo.java
@@ -0,0 +1,14 @@
+package com.edwinab.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadEjemplo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejemplo1);
+    }
+}
--- app/src/main/java/com/edwinab/app1/MainActivity.java
+++ app/src/main/java/com/edwinab/app1/MainActivity.java
@@ -1,13 +1,24 @@
 package com.edwinab.app1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,9 +27,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +40,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
+                .setDrawerLayout(drawerLayout)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(MainActivity.this);
     }
 
     @Override
@@ -53,5 +67,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.nav_inicio){
+
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.toast_content, (ViewGroup) findViewById(R.id.cardToast));
+
+
+            CardView cardView = layout.findViewById(R.id.cardToast);
+            cardView.setBackgroundColor(getResources().getColor(R.color.colorAmarillo));
+
+
+            TextView tvToast = layout.findViewById(R.id.texto_de_mi_toast);
+            tvToast.setTextColor(getResources().getColor(R.color.black));
+
+            tvToast.setText("Hiciste click en INICIO");
+
+
+
+
+            Toast mitoast = new Toast(MainActivity.this);
+            mitoast.setView(layout);
+            mitoast.setDuration(Toast.LENGTH_LONG);
+            mitoast.show();
+
+
+            startActivity(new Intent(this, ActividadEjemplo.class));
+
+
+        }
+
+        //Cerrar el Menu Drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
